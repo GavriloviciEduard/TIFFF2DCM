ca385d73d4b456d47c808cd1d60af044ac0b7a59
unknown <eddiegavrilovici@gmail.com>
2019-08-14 15:12:24 +0300

    minor

--- TIFF2DCM/TIFF2DCM/main.cpp
+++ TIFF2DCM/TIFF2DCM/main.cpp
@@ -4,10 +4,17 @@
 int wmain(int argc, wchar_t *argv[])
 {
 
-	Tiff2Dcm converter = Tiff2Dcm();
-	std::wstring name(argv[0]);
-	converter.convertBMPtoDCM(argv[0],argv[1]);
+	if (argv[0] && argv[1] && argv[2])
+	{
+		Tiff2Dcm converter = Tiff2Dcm();
+		std::wstring name(argv[0]);
+		converter.convertBMPtoDCM(argv[1], argv[2]);
 
+		return 0;
+	}
+
+	else
+		return - 1; //error
 }
 
 	
